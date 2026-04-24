43cabf1c01d706302f608ae586af3b0db4b67e0c
rafergiomrclno <120321690+rafergiomrclno@users.noreply.github.com>
2026-04-24 18:30:18 +0700

    Create and drop multiple tables in bronze layer

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -10,17 +10,10 @@ Script Purpose:
 ========================================================
 */
 
-
-
--- Recreate an existing table
-IF OBJECT_ID ('Table_name', 'U') IS NOT NULL 
-	DROP TABLE Table_name;
-
--- Apply this before creating a new table
-
-
-
 -- CRM
+-- Create crm_cust_info file in bronze layer
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL 
+	DROP TABLE bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
 	cst_id				INT,
 	cst_key				NVARCHAR(50),
@@ -31,6 +24,9 @@ CREATE TABLE bronze.crm_cust_info (
 	cst_create_date		DATE
 );
 
+-- Create crm_prd_info file in bronze layer
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL 
+	DROP TABLE bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info (
 	prd_id			INT,
 	prd_key			NVARCHAR(50),
@@ -41,6 +37,9 @@ CREATE TABLE bronze.crm_prd_info (
 	prd_end_dt		DATETIME
 );
 
+-- Create crm_sales_details file in bronze layer
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL 
+	DROP TABLE bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num		NVARCHAR(50),
 	sls_prd_key		NVARCHAR(50),
@@ -54,18 +53,26 @@ CREATE TABLE bronze.crm_sales_details (
 );
 
 -- ERP
+-- Create erp_cust_az12 file in bronze layer
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL 
+	DROP TABLE bronze.erp_cust_az12;
 CREATE TABLE bronze.erp_cust_az12 (
 	cid		NVARCHAR(50),
 	bdate	DATE,
 	gen		NVARCHAR(50)
 );
 
-
+-- Create erp_loc_a101 file in bronze layer
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL 
+	DROP TABLE bronze.erp_loc_a101;
 CREATE TABLE bronze.erp_loc_a101 (
 	cid		NVARCHAR(50),
 	cntry	NVARCHAR(50)
 );
 
+-- Create erp_px_cat_g1v2 file in bronze layer
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL 
+	DROP TABLE bronze.erp_px_cat_g1v2;
 CREATE TABLE bronze.erp_px_cat_g1v2 (
 	id			NVARCHAR(50),
 	cat			NVARCHAR(50),
